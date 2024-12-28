1d4266d4e1a2e44d1af74f545048b42deaa54da6
madeofstown <33820964+madeofstown@users.noreply.github.com>
2024-12-27 16:13:07 -0800

    Update opijammy-prep.sh

--- opijammy-prep.sh
+++ opijammy-prep.sh
@@ -1,14 +1,9 @@
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y libgpiod-dev libyaml-cpp-dev libbluetooth-dev openssl libssl-dev libulfius-dev liborcania-dev python3.10-venv git
 sudo apt install -y wget python3 python3-pip python3-wheel python3-venv g++ zip git \
-                           ca-certificates libgpiod-dev libyaml-cpp-dev libbluetooth-dev \
-                           libusb-1.0-0-dev libulfius-dev liborcania-dev libssl-dev pkg-config && \
+                           ca-certificates libgpiod-dev libi2c-dev libyaml-cpp-dev libbluetooth-dev \
+                           libusb-1.0-0-dev libulfius-dev liborcania-dev libssl-dev pkg-config
 wget -O get-platformio.py https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py
 python3 get-platformio.py
-sudo mkdir -p /usr/local/bin
-sudo ln -s ~/.platformio/penv/bin/platformio /usr/local/bin/platformio
-sudo ln -s ~/.platformio/penv/bin/pio /usr/local/bin/pio
-sudo ln -s ~/.platformio/penv/bin/piodebuggdb /usr/local/bin/piodebuggdb
 mkdir ~/meshtastic-source
 cd ~/meshtastic-source
 git clone https://github.com/meshtastic/firmware.git
@@ -16,6 +11,7 @@ cd firmware/
 git fetch --tags
 latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 git checkout $latestTag
-. ~/.platformio/penv/bin/activate
-sudo ./bin/build-native.sh
+source ~/.platformio/penv/bin/activate
+./bin/build-native.sh
+deactivate
 sudo ./bin/native-install.sh
